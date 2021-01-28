e64bc79abc3a3cef7083b431f634cb287ae2e175
lorenzo-boldrini <56259768+lorenzo-boldrini@users.noreply.github.com>
2021-01-28 13:28:24 +0100

    set up string controller

---
+++ Assets/Scripts/pakage/ScriptController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScriptController : MonoBehaviour
+{
+    string funzione;
+
+    public Text testoDaRipulire;
+
+    public void AddFunction(string Stringa)
+    {
+        funzione = Stringa;
+        print(funzione);
+        testoDaRipulire.text = "";
+
+        doFunction();
+    }
+
+    void doFunction()
+    {
+        if(funzione == "move forword")
+        {
+            MoveForword();
+        }
+
+        funzione = "";
+    }
+    void MoveForword()
+    {
+        transform.position = transform.position + Vector3.forward; 
+    }
+}
