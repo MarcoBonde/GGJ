59fb8db326377fa20806640847ccc42d39fb6f17
lorenzo-boldrini <56259768+lorenzo-boldrini@users.noreply.github.com>
2021-01-29 20:16:56 +0100

    cambio pacco setup

---
+++ Assets/Scripts/pakage/cambia_formaPacco.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cambia_formaPacco : MonoBehaviour
+{
+    public GameObject[] pacchi;
+    int UltimoPacco;
+    GameObject packRef;
+    // Start is called before the first frame update
+    void Start()
+    {
+        packRef = GetComponentInChildren<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CambiaPacco()
+    {
+        Destroy(packRef);
+        packRef = Instantiate(pacchi[Random.Range(0, pacchi.Length)], transform.position, Quaternion.identity);
+    }
+}
