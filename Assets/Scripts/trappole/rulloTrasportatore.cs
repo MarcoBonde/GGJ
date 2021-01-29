791ad129df450579a4074c8cffc2c34f6b0c0896
lorenzo-boldrini <56259768+lorenzo-boldrini@users.noreply.github.com>
2021-01-29 16:54:40 +0100

    rulli

---
+++ Assets/Scripts/trappole/rulloTrasportatore.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rulloTrasportatore : MonoBehaviour
+{
+    [SerializeField] float speed;
+    Material texture;
+    // Start is called before the first frame update
+    void Start()
+    {
+        texture = GetComponent<MeshRenderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 textureOffset = new Vector2(0, (Time.time * speed) / 7);
+        texture.mainTextureOffset = textureOffset;
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag != "rulli")
+        {
+            Rigidbody BanbinoBislacco = collision.gameObject.GetComponent<Rigidbody>();
+
+            BanbinoBislacco.velocity = transform.forward* speed;
+        }
+    }
+}
