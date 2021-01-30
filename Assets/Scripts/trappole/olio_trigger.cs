a7c759373e120f6cefcdd1a091ed532badd98fa5
lorenzo-boldrini <56259768+lorenzo-boldrini@users.noreply.github.com>
2021-01-30 19:30:04 +0100

    game manager

---
+++ Assets/Scripts/trappole/olio_trigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class olio_trigger : MonoBehaviour
+{
+    Rigidbody PlayerRigidoby;
+    CharacterController PCC;
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            PlayerRigidoby = other.GetComponent<Rigidbody>();
+            PCC = other.GetComponent<CharacterController>();
+            PCC.SimpleMove(other.transform.forward * 10);
+        }
+    }
+}
