92083737038492b2281c1e2e54bef62d926859ee
laura-deguio <laura-deguio@users.noreply.github.com>
2021-01-30 12:47:46 +0100

    Game Manager

---
+++ Assets/Scripts/Player Scripts/GameManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+
+    public GameObject package;
+    //public GameObject forklift;
+    public GameObject oil;
+
+    public int counterPackage;
+    //public int counterForklift;
+    public int counterOil;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    public void StunPackage()
+    {
+        Instantiate(package, spawnPoints[counterPackage]);
+
+        if(counterPackage <= spawnPoints.Length -1)
+        {
+            counterPackage++;
+        }
+        else
+        {
+            counterPackage = 0;
+        }
+    }
+
+    /*
+    public void StunForklift()
+    {
+        Instantiate(package, spawnPoints[counterForklift]);
+
+        if (counterForklift <= spawnPoints.Length - 1)
+        {
+            counterForklift++;
+        }
+        else
+        {
+            counterForklift = 0;
+        }
+    }
+    */
+
+    public void StunOil()
+    {
+        Instantiate(package, spawnPoints[counterOil]);
+
+        if (counterOil <= spawnPoints.Length - 1)
+        {
+            counterOil++;
+        }
+        else
+        {
+            counterOil = 0;
+        }
+    }
+}
