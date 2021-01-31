a533c7d3b69de5ec12d82ce18e3f33345b2c19d6
laura-deguio <laura-deguio@users.noreply.github.com>
2021-01-31 01:17:00 +0100

    CIao

---
+++ Assets/Scripts/Player Scripts/MenuController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class MenuController : MonoBehaviour
+{
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Retry()
+    {
+        print("Ciao");
+        SceneManager.LoadScene(3);
+    }
+
+    public void GameOver()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+    public void Win()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void SecondCredits()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
