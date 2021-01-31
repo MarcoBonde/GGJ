40a46b553c49de294fb2e982303271f88597a1c6
lorenzo-boldrini <56259768+lorenzo-boldrini@users.noreply.github.com>
2021-01-31 01:08:30 +0100

    twich light controll

---
+++ Assets/Scripts/trappole/trigger_light.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class trigger_light : MonoBehaviour
+{
+    public UnityEvent turnOnLight;
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            if(Input.GetKeyDown(KeyCode.E))
+            turnOnLight.Invoke();
+        }
+    }
+}
